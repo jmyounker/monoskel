b0f12c461a38f85e6d9de0e928332c2294029504
Jeff Younker <jeff@drinktomi.com>
2019-09-01 12:14:17 +0200

    Build/install flake8 hook using Bazel.

--- cell/ci/hooks/lint-python3-flake8.py
+++ cell/ci/hooks/lint-python3-flake8.py
@@ -1,25 +1,17 @@
-#!/usr/bin/env python3
-
-
 """Ensure that Python files pass flake8 checks.
 
     Usage: lint-python3-flake8 [FILES]+
 
     Runs `flake8` on the requested files. Any failure results in a non-zero return code.
 
-    """
+"""
 
 
 import os
-import stat
 import sys
 
 
 MAX_SHEBANG_LINE = 10000
-
-
-def is_executable(path):
-    return bool(os.stat(path).st_mode & (stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH))
 
 
 def is_python_file(path):
@@ -33,13 +25,30 @@ def is_python_file(path):
     return "python3" in first_line[2:]
 
 
+def find_in_path(predicate, path):
+    if predicate(path):
+        return path
+    dirname, basename = os.path.split(path)
+    if dirname == path:
+        return None
+    return find_in_path(predicate, dirname)
+
+
+def contains_file(filename):
+    def predicate(path, filename=filename):
+        return os.path.exists(os.path.join(path, filename))
+
+    return predicate
+
+
 def main(args):
-    bin_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
-    project_root = os.path.dirname(bin_dir)
-    config = os.path.join(project_root, "pyproject.toml")
     python_files = [x for x in args[1:] if is_python_file(x)]
     if not python_files:
         return 0
+    root = find_in_path(contains_file("pyproject.toml"), os.path.abspath(args[0]))
+    if root is None:
+        return 1
+    config = os.path.join(root, "pyproject.toml")
     os.execvp("flake8", ["flake8", "--config", config] + python_files)
 
 
